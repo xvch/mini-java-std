0076c5ff7e9d57564549eda7fdf129c99ac8e375
xvch <3345929692@qq.com>
2016-03-08 16:28:44 +0800

    Update Math.java
    为Math类添加min，max重载函数

--- src/math/Math.java
+++ src/math/Math.java
@@ -1,11 +1,41 @@
-class Math {
-	static byte max(byte x, byte y) {
+public final class Math {
+	
+	/* max方法：用于返回两个同类型数据的较大值 */
+	public static byte max(byte x, byte y) {
+		return x>=y ? x : y;
 	}
-	static short max(short x, short y) {
+	public static short max(short x, short y) {
+		return x>=y ? x : y;
 	}
-	static int max(int x, int y) {
+	public static int max(int x, int y) {
+		return x>=y ? x : y;
 	}
-	static long max(long x, long y) {
+	public static long max(long x, long y) {
+		return x>=y ? x : y;
+	}
+	public static float max(float x, float y) {
+	}
+	public static double max(double x, double y) {
+
 	}
 	
+	/* min方法：用于返回两个同类型数据的较小值 */
+	public static byte min(byte x, byte y) {
+		return x<=y ? x : y;
+	}
+	public static short min(short x, short y) {
+		return x<=y ? x : y;
+	}
+	public static int min(int x, int y) {
+		return x<=y ? x : y;
+	}
+	public static long min(long x, long y) {
+		return x<=y ? x : y;
+	}
+	public static float min(float x, float y) {
+		
+	}
+	public static double min(double x, double y) {
+		
+	}
 }
